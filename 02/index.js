f7c709da9bed10ade9711882c5b159364663abf9
Rascal_Two <therealrascaltwo@gmail.com>
2021-12-02 23:21:21 -0500

    Add 02 Solution

--- 02/index.js
+++ 02/index.js
@@ -3,6 +3,11 @@ const greeting = document.getElementById("greeting")
 const christmasBtn = document.getElementById("christmas")
 const snowBtn = document.getElementById("snow")
 
+[christmasBtn, snowBtn].forEach(btn => btn.addEventListener('change', ({ target: { checked, value } }) => {
+	if (!checked) return;
+	body.className = value;
+	greeting.textContent = checked && value === 'christmas' ? '🎅 Merry Christmas!' : '☃️ Happy Holidays!'
+}));
 // Task:
 //- Add the functionality to switch the theme between 'Christmas' and 'snow'.
 
