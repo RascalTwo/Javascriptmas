828d03fc5ffc23812822b55abadf5ad5568f520a
Rascal_Two <therealrascaltwo@gmail.com>
2021-12-04 12:56:40 -0500

    Add 04 Solution

--- 04/index.js
+++ 04/index.js
@@ -1,6 +1,40 @@
 const items = ["Candles", "Decorations", "Chocolate"]
 const checklist = document.getElementById("checklist")
 
+function renderItems(items){
+	checklist.innerHTML = '';
+	checklist.appendChild(items.reduce((fragment, item, i) => {
+		const uid = Date.now() + i;
+
+		const div = document.createElement('div');
+		div.className = 'checklist-item';
+		div.innerHTML = `
+			<input id="${uid}" type="checkbox">
+			<label class="strikethrough" for="${uid}">${item}</label>
+		`;
+
+		fragment.appendChild(div);
+		return fragment;
+	}, document.createDocumentFragment()));
+
+
+	const ui = document.createElement('form');
+	ui.className = 'checklist-item';
+	ui.innerHTML = `<input id="new-item" required placeholder="New Item" /><button>Add</button>`
+	ui.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		const input = e.currentTarget.elements[0];
+		items.push(input.value);
+		input.value = '';
+
+		renderItems(items);
+	});
+	checklist.appendChild(ui);
+}
+
+renderItems(items);
+
 // Task:
 // - For each item in the items array, create a div with a class of "checklist-item", which contains a checkbox input and corresponding label.
 // - Make sure that the shopping list can render a checkbox for all the items, even if new items are added to the items array.
