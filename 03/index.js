13c96abb604850d3c560734d2e509e47db7cbadd
Rascal_Two <therealrascaltwo@gmail.com>
2021-12-03 21:37:35 -0500

    Add 03 Solution

--- 03/index.js
+++ 03/index.js
@@ -1,11 +1,25 @@
 const greeting = document.getElementById("greeting")
 const btn = document.getElementById("btn")
 
+function isNewYears(){
+  const now = new Date();
+  return now.getMonth() === 11 && now.getDate() === 31;
+}
+
+let christmassy = false;
 function fix() {
+  christmassy = !christmassy;
+  const [textContent, fontFamily] = christmassy
+    ? [`🎅 ${isNewYears() ? 'Happy New Years' : 'Merry Christmas'}! ☃️`, 'Mountains of Christmas']
+    : ['🎃 Happy Halloween! 👻', 'Rubik Beastly'];
+
+  greeting.textContent = textContent
+  greeting.style.fontFamily = fontFamily
   // Task:
   // - Write a function to fix the UI problems with this Christmas message (make it Christmassy!)
   // - Run the function when the Fix button is clicked.
 }
+btn.addEventListener('click', fix);
 
 //Stretch goals:
 // - Add an extra theme, and the option to switch between them.
