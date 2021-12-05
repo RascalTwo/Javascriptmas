a13513f9bee1f6b9234b700d04453962ce50d171
Rascal_Two <therealrascaltwo@gmail.com>
2021-12-05 13:49:43 -0500

    Add 05 Solution

--- 05/index.js
+++ 05/index.js
@@ -11,6 +11,51 @@ const greetings = [
     "Jingle all the way!",
 ]
 
+const randomFrom = arr => arr[Math.floor(Math.random() * greetings.length)]
+
+function writeGreeting() {
+    greetingDisplay.textContent = randomFrom(greetings);
+}
+
+const card = document.getElementById('card');
+document.getElementById('ctm').addEventListener('click', () => {
+    const customFromTo = confirm('Add "From" and "To" fields?')
+    let from, to;
+    if (customFromTo) {
+        from = prompt('From: ');
+        if (!from) return;
+
+        to = prompt('To: ');
+        if (!to) return;
+    }
+
+    const customGreeting = confirm('Add custom greeting message?')
+    let greeting;
+    if (customGreeting) {
+        greeting = prompt('From: ');
+        if (!greeting) return;
+    }
+    else {
+        greeting = randomFrom(greetings);
+    }
+
+    for (const [label, value] of Object.entries({ From: from, To: to })) {
+        const className = label.toLowerCase();
+        let node = document.getElementsByClassName(className)[0];
+        if (!value) {
+            if (node) node.remove();
+            continue;
+        }
+        if (!node) {
+            node = document.createElement('span');
+            node.className = className;
+            card.insertAdjacentElement('afterbegin', node);
+        }
+        node.textContent = `${label}: ${value}`;
+    }
+    greetingDisplay.textContent = greeting;
+});
+
 // Task:
 // Write a function to display a random greeting in the card.
 
