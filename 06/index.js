4c073efd13724e5e5fc28a2feb06bbe369346898
Rascal_Two <therealrascaltwo@gmail.com>
2021-12-06 12:59:56 -0500

    Add 06 Solution

--- 06/index.js
+++ 06/index.js
@@ -2,7 +2,24 @@ const greeting = document.getElementById("greeting")
 const christmassifierBtn = document.getElementById("christmassifierBtn")
 christmassifierBtn.addEventListener("click", christmassifyName)
 
+let removing;
+let music = new Audio('https://www.coralesangaudenzio.it/Audio/cn09/mg15.mp3');
+function deChristmassify() {
+  christmassifierBtn.textContent = 'Christmassify';
+  greeting.classList.remove('christmassified');
+  music.pause();
+  music.fastSeek(0);
+}
+
 function christmassifyName() {
+  if (greeting.classList.toggle('christmassified')) {
+    if (removing) clearTimeout(removing);
+    music.play();
+    removing = setTimeout(deChristmassify, music.duration * 1000 || 5000);
+    christmassifierBtn.textContent = 'De-christmassify';
+  } else {
+    deChristmassify();
+  }
    // Task:
   // - Add christmassify class to greeting.
   //- Check whether christmassifierBtn has christmassify on it. If so, change text to "De-christmassify", if not, change text to "Christmassify"
