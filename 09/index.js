0f9f03cfd343894b78331e1b75a79afa1762abc4
Rascal_Two <therealrascaltwo@gmail.com>
2021-12-09 13:58:02 -0500

    Add 09 Solution

--- 09/index.js
+++ 09/index.js
@@ -1,5 +1,12 @@
 const meter = document.getElementById("meter")
 
+
+const now = new Date();
+const christmas = new Date(now.getFullYear(), 11, 25);
+const remaining = christmas - now;
+const days = remaining / (1000 * 3600 * 24);
+meter.value = Math.min(25 - days, 25);
+
 // Task:
 // Write a function to wire up the festivity loader to reflect how many days are remaining until Christmas!
 
