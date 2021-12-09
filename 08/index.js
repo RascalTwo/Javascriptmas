6747e2b540000b24c922b0c483ab15102a4f391e
Rascal_Two <therealrascaltwo@gmail.com>
2021-12-08 21:39:41 -0500

    Add 08 Solution

--- 08/index.js
+++ 08/index.js
@@ -1,23 +1,50 @@
 const playBtn = document.getElementById("play-btn")
 const pauseBtn = document.getElementById("pause-btn")
 const stopBtn = document.getElementById("stop-btn")
-const audio = new Audio('bells.mp3');
-playBtn.addEventListener("click", play)
-pauseBtn.addEventListener("click", pause)
-stopBtn.addEventListener("click", stop)
+const volumeRng = document.getElementById("volume-rng")
+const soundSlt = document.getElementById("sound-slt")
+const bell = (() => {
+	const bell = document.getElementById("bell");
+	let timeout;
 
-function play() {
-    audio.play()
-}
- 
- function pause() {
-    audio.pause()
-}
+	const stop = () => {
+		clearTimeout(timeout);
+		bell.style.animationPlayState = 'paused';
+	}
 
-function stop() {
-    audio.pause();
-    audio.currentTime = 0;
-}
+	const start = () => {
+		clearTimeout(timeout);
+		bell.style.animationPlayState = 'running';
+		stopAnimation = setTimeout(stop, (audio.duration - audio.currentTime) * 1000);
+	}
+
+	return { stop, start };
+})();
+bell.stop();
+
+
+let audio = new Audio(soundSlt.value);
+audio.volume = Number(volumeRng.value) / 100;
+
+
+playBtn.addEventListener('click', () => {
+	audio.play();
+	bell.start();
+});
+pauseBtn.addEventListener('click', () => {
+	audio.pause();
+	bell.stop();
+});
+stopBtn.addEventListener('click', () => {
+	audio.pause();
+	audio.currentTime = 0;
+	bell.stop();
+});
+volumeRng.addEventListener('input', ({ target: { value }}) => audio.volume = Number(value) / 100);
+soundSlt.addEventListener('change', ({ target: { value }}) => {
+	audio.src = value;
+	bell.stop();
+});
 
 // Task:
 // - Animate the bell so that it looks like it is ringing when the music is playing, and stops when the music is paused or stopped. 
