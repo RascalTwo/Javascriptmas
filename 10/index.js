02106020fe57faf3c76315eea24295cca7d933ad
Rascal_Two <therealrascaltwo@gmail.com>
2021-12-10 20:18:26 -0500

    Add 10 Solution

--- 10/index.js
+++ 10/index.js
@@ -1,6 +1,7 @@
 const previous = document.getElementById("previous")
 const next = document.getElementById("next")
 const img = document.getElementById("img")
+const destImg = document.getElementById("dest-img")
 const imgs = [
     {src: "imgs/village.jpg",
     alt: "christmas village at night with snow and christmas tree"},
@@ -11,6 +12,25 @@ const imgs = [
 
 let imgNum = 0
   
+const changeImage = (amount) => {
+  [previous, next].forEach(btn => btn.disabled = true);
+
+  imgNum = (imgNum + amount) % imgs.length;
+  if (imgNum < 0) imgNum = imgs.length - 1;
+
+  Object.assign(destImg, imgs[imgNum]);
+  img.classList.add('transition');
+  setTimeout(() => {
+    Object.assign(img, imgs[imgNum]);
+    img.classList.remove('transition');
+
+    [previous, next].forEach(btn => btn.disabled = false);
+  }, 1000);
+}
+
+previous.addEventListener('click', changeImage.bind(null, -1))
+next.addEventListener('click', changeImage.bind(null, 1))
+
 // Task:
 // - Wire up the buttons to switch through the images in the imgs array. 
 // - Make sure that the gallery works no matter how many images are added.
